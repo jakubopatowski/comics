09746f06a6d5c6d81d021c7c8349a268304b9d07
Jakub Opatowski <jakub.opatowski@gmail.com>
2020-11-12 14:59:38 +0100

    Added Boost library support

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
-#include<iostream>
+#include <iostream>
+#include <chrono>
 using namespace std;
+
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/steady_timer.hpp>
+using namespace boost::asio;
 
 int main()
 {
-    cout << "Hello world!\n";
+    io_service ioservice;
+
+    steady_timer timer{ioservice, std::chrono::seconds{3}};
+    timer.async_wait([](const boost::system::error_code &ec)
+    {std::cout << "3 sec\n";});
+ 
+    ioservice.run();
     return 0;
 }
