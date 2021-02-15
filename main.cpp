26ef81c1090289f9bd7a03b8cf56710df7f792fc
Jakub Opatowski <jakub.opatowski@gmail.com>
2021-02-15 15:39:22 +0100

    Brand new configuration and Catch2 test integration

--- main.cpp
+++ main.cpp
@@ -1,27 +1,7 @@
 #include <iostream>
-#include <memory>
-#include <string>
-using namespace std;
 
-#include "event.hpp"
-#include "foo.hpp"
-
-int main() {
-    std::unique_ptr< FooFoo > foo( new FooFoo( 1 ) );
-    cout << foo->getNo() << '\n';
-    foo = std::make_unique< FooFoo >( 2 );
-    cout << ( *foo ).getNo() << '\n';
-    // cout << *foo.getNo() << '\n'; // error, cheeck operator precedence
-
-    if ( foo ) {
-        cout << "Foo object exists, it holds no. " << ( *foo ).getNo() << '\n';
-    }
-    foo.reset();
-    
-    if ( !foo ) {
-        cout << "There is no Foo object." << '\n';
-    }
-
-    cout << "Main function ends here." << '\n';
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello, World!\n";
     return 0;
 }
