513b7d7e479ddd3f7ad2741a2879b9ffdd5ec850
Jakub Opatowski <jakub.opatowski@gmail.com>
2021-02-19 08:50:22 +0100

    Catch2 update; Added Open GLFW and sqlite3 library

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include "sqlite3.h"
 
-int main(int argc, char *argv[])
-{
-    std::cout << "Hello, World!\n";
-    return 0;
+int add(int x, int y) { return x + y; }
+
+int main(int argc, char *argv[]) {
+  std::cout << "Hello, World!\n";
+  std::cout << add(1, 1) << std::endl;
+  return 0;
 }
+ 
