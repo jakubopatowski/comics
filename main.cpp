a2372d06a085156201fe59fa40fef7622ed211b1
Jakub Opatowski <jakub.opatowski@gmail.com>
2021-02-27 12:24:50 +0100

    Small adjustments to cmake build

--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
-#include <iostream>
 #include "sqlite3.h"
-
-int add(int x, int y) { return x + y; }
+#include <iostream>
+#include <vector>
 
 int main(int argc, char *argv[]) {
-  std::cout << "Hello, World!\n";
-  std::cout << add(1, 1) << std::endl;
-  return 0;
+    return 0;
 }
- 
